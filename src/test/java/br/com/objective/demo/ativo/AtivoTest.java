9c2a08c2db7bbc149cf87355eb7f96cb8861d26e
Herbert Weigert Venancio <herbert.venancio@objective.com.br>
2019-10-31 15:05:14 -0300

    added assertion to test

--- src/test/java/br/com/objective/demo/ativo/AtivoTest.java
+++ src/test/java/br/com/objective/demo/ativo/AtivoTest.java
@@ -2,22 +2,26 @@ package br.com.objective.demo.ativo;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AtivoTest {
 
     @Test
     public void dadoUmNomeValido_quandoInstanciarUmNovoAtivo_deveriaSerInstanciado(){
-        new Ativo("MCD-X1000");
+        Ativo ativo = new Ativo("MCD-X1000");
+        assertThat(ativo.getNome()).isEqualTo("MCD-X1000");
     }
 
-    @Test void dadoUmNomeVazio_quandoInstanciarUmNovoAtivo_deveriaLancarExcecao(){
+    @Test
+    public void dadoUmNomeVazio_quandoInstanciarUmNovoAtivo_deveriaLancarExcecao(){
         assertThrows(NomeAtivoInvalido.class, () -> {
            new Ativo("");
         });
     }
 
-    @Test void dadoUmNomeNulo_quantoInstanciarUmNovoAtivo_deveriaLancarExcecao(){
+    @Test
+    public void dadoUmNomeNulo_quantoInstanciarUmNovoAtivo_deveriaLancarExcecao(){
         assertThrows(NomeAtivoInvalido.class, () -> {
             new Ativo(null);
         });
